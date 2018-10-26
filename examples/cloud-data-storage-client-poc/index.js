107705404bfba34736a6b62995554a90af6e89b6
navindu amarakoon <navanav3@gmail.com>
2018-10-26 11:44:26 +0530

    Remove redundant comment

--- examples/cloud-data-storage-client-poc/index.js
+++ examples/cloud-data-storage-client-poc/index.js
@@ -2,32 +2,26 @@
  * Before running this file make sure GOOGLE_APPLICATION_CREDENTIALS variable is set
  */
 
-const Storage = require('@google-cloud/storage');
+const Storage = require("@google-cloud/storage");
 const gcs = new Storage();
 
 gcs.interceptors.push({
   request: reqOpts => {
     reqOpts.uri = reqOpts.uri.replace(
-      'https://www.googleapis.com',
-      'http://localhost:8080'
+      "https://www.googleapis.com",
+      "http://localhost:8080"
     );
-    // console.log(JSON.stringify(reqOpts));
     return reqOpts;
-  },
+  }
 });
 
 gcs
-  .createBucket('test-bucket')
+  .createBucket("test-bucket")
   .then(async res => {
     try {
       const bucketList = await gcs.getBuckets();
 
-      const bucket = await gcs.bucket('test-bucket').get();
-
-      // this endpoint doesn't work correctly
-      // const uploadRes = await gcs.bucket('test-bucket').upload('./test.txt');
-
-      // await gcs.bucket('test-bucket').delete();
+      const bucket = await gcs.bucket("test-bucket").get();
     } catch (err) {
       console.log(`${err.message}`);
     }
