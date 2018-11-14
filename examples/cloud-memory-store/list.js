47303dec7979ad9b7d7f698e2f1e8f095a8a0419
Fizaan Khan <freak.fizaan@gmail.com>
2018-11-14 21:28:16 +0530

    Remove postman tokens

--- examples/cloud-memory-store/list.js
+++ examples/cloud-memory-store/list.js
@@ -1,15 +1,6 @@
 var request = require('request');
 
-var options = {
-  method: 'GET',
-  url: 'http://localhost:7070/v1beta1/projects/testProject/locations/us-east-1',
-  headers: {
-    'Postman-Token': 'e928a93b-efca-44a7-9ac7-69120cef3224',
-    'Cache-Control': 'no-cache',
-  },
-};
-
-request(options, function(error, response, body) {
+request(function(error, response, body) {
   if (error) throw new Error(error);
 
   console.log(body);
