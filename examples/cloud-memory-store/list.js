296a0f2a8af2322371fc4e35c6549fb0c2853a69
Łukasz Myśliwiec <lmysliwiec10@gmail.com>
2018-11-14 13:11:10 +0100

    Remove token from list.js

--- examples/cloud-memory-store/list.js
+++ examples/cloud-memory-store/list.js
@@ -3,10 +3,7 @@ var request = require('request');
 var options = {
   method: 'GET',
   url: 'http://localhost:7070/v1beta1/projects/testProject/locations/us-east-1',
-  headers: {
-    'Postman-Token': 'e928a93b-efca-44a7-9ac7-69120cef3224',
-    'Cache-Control': 'no-cache',
-  },
+  headers: { 'Cache-Control': 'no-cache' },
 };
 
 request(options, function(error, response, body) {
