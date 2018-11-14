607f8607a6ce6aec709307441af64975e4dc87d7
isman3 <35353292+isman3@users.noreply.github.com>
2018-11-14 13:03:06 +0700

    Update get.js
    Remove postman token get.js

--- examples/cloud-memory-store/get.js
+++ examples/cloud-memory-store/get.js
@@ -5,7 +5,6 @@ var options = {
   url:
     'http://localhost:7070/v1beta1/projects/testProject/locations/us-east-1/instances/redisTest123',
   headers: {
-    'Postman-Token': '8be7f29e-cff0-4b95-a43a-6eb347bc0e37',
     'Cache-Control': 'no-cache',
   },
 };
