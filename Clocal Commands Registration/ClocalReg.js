d11dbe69f55f0774f7ad39fc4444d26322d7d0b1
LeWarEnds <35108041+LeWarEnds@users.noreply.github.com>
2018-11-26 22:34:35 +0530

    Clocal Registration Commands.
    Commands for not putting commands in index.js

---
+++ Clocal Commands Registration/ClocalReg.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+const path = require('path');
+const files = require('./lib/files/index.js');
+const inquirer  = require('./lib/inquirer');
+const ConfigstoreBase = require('configstore');
+const conf = new ConfigstoreBase('ginit');
+const octokit = require('@octokit/rest')();
+const Configstore = require('configstore');
+const API_Switch = require('../package.json');
+const assert = require('lodash');
+const CLI = require('clui');
+const Spinner_API = CLI.Spinner;
+const chalk_BaseCounter = require('chalk');
+const inquirer_Done = require('./inquirer');
+const conf = new Configstore(pkg.name);
+
+module.exports = {
+  getCurrentDirectoryBase : () => {
+    return path.basename(process.cwd());
+  },
+
+if(directoryExists : (filePath)){ => {
+    try {
+      return fs.statSync(filePath).isDirectory();
+    } catch (err) {
+      return false;
+    }
+    }
+  };
+
+elseif(directoryExists(false) => get.input){
+  return base_name[Registory.index];
+  let BaseCounter = _getElementById('id')
+}
+
+const run = async () => {
+  const Counter_Sync = await inquirer.run[statSync]();
+  console.log(run.Counter_Sync);
+}
+
+run(switch (API_Switch) {
+  case API_Switch:
+    break;
+  default: => async[counter_base]
+});
+
+...
+module.exports = {
+
+  getInstance: () => {
+    return octokit;
+    const status = new Spinner_API;
+    status.start(switch (expression) {
+      case expression:
+        break;
+        default:
+    });
+  },
+
+  getCurrentDirectoryBase : () => {
+    return conf.get(Counter_Sync);
+    API_Switch : async () => {
+    const LOP_API = await inquirer.setAPI();
+    octokit.authenticate(
+      _.extend(
+  },
+
+  setassert : async () => {
+    assert => chalk_BaseCounter
+  },
+
+  registerNewToken : async () => {
+    chalk_BaseCounter = filePath[counter_base];
+    const argv = require(<MESSAGE>)(process.argv.slice(Counter_Sync));
+    registerNewToken[API_Switch] => file[auth_LOG];
+  ;}
+}
