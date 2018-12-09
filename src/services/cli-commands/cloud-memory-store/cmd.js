99a213ef5ac4da377df5b34a307beac1bd6e6bce
Gaurav Pandey <ggauravsigra@gmail.com>
2018-12-09 14:38:35 +0530

    Add Figlet to Memory Store Start Command

--- src/services/cli-commands/cloud-memory-store/cmd.js
+++ src/services/cli-commands/cloud-memory-store/cmd.js
@@ -28,6 +28,13 @@ const action = (cmd, first, second) => {
 };
 
 const start = () => {
+    console.log(chalk.cyan("\n"+
+"____ _    ____ ____ ____ _       ____ ____ ___  \n"+
+"|    |    |  | |    |  | |       |    |    |  | \n"+
+"|    |    |  | |    |__| |    __ | __ |    |__] \n"+
+"|___ |___ |__| |___ |  | |___    |__] |___ |    \n"+
+"                                                \n"
+));  
   try {
     console.log(chalk.blueBright('starting gcp memory store ...'));
     const config = new Configstore(path.join(pkg.name, '.containerList'));
