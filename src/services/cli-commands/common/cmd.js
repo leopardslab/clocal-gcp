4bcd64cbfd0a942663aae3bcfa2f3a4dad5afbcc
Gaurav Pandey <ggauravsigra@gmail.com>
2018-12-10 10:24:24 +0530

    Add Common Figlet File

---
+++ src/services/cli-commands/common/cmd.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const chalk = require('chalk');
+function figlet(){
+    return chalk.cyan("\n"+
+    "____ _    ____ ____ ____ _       ____ ____ ___  \n"+
+    "|    |    |  | |    |  | |       |    |    |  | \n"+
+    "|    |    |  | |    |__| |    __ | __ |    |__] \n"+
+    "|___ |___ |__| |___ |  | |___    |__] |___ |    \n"+
+    "                                                \n"
+  );
+}
+module.exports = {
+  commandName: '',
+  figlet:figlet,
+};
