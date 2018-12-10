65b6ab9d5b77a85e31f73ea5cf60a2772e81f941
Gaurav Pandey <ggauravsigra@gmail.com>
2018-12-10 10:26:24 +0530

    Update Cloud-function to call common modules

--- src/services/cli-commands/cloud-function/cmd.js
+++ src/services/cli-commands/cloud-function/cmd.js
@@ -9,7 +9,7 @@ const fs = require('fs');
 const Configstore = require('configstore');
 const path = require('path');
 const pkg = require('../../../../package.json');
-
+const common = require('../common/cmd');
 const dockerImage = `dilantha111/clocal-gcp-function:0`;
 const defaultPort = 8000;
 
@@ -110,13 +110,7 @@ const deleteFunc = functionName => {
 };
 
 const start = () => {
-    console.log(chalk.cyan("\n"+
-"____ _    ____ ____ ____ _       ____ ____ ___  \n"+
-"|    |    |  | |    |  | |       |    |    |  | \n"+
-"|    |    |  | |    |__| |    __ | __ |    |__] \n"+
-"|___ |___ |__| |___ |  | |___    |__] |___ |    \n"+
-"                                                \n"
-));
+   console.log(common.figlet());
   try {
     console.log(chalk.blueBright('starting gcp function ...'));
     const config = new Configstore(path.join(pkg.name, '.containerList'));
