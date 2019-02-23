d2235832ffdd34f151afe553815f4dd1b00b2407
Shehan Dhaleesha <sonodewcomplex@gmail.com>
2019-02-23 19:35:36 +0530

    Update list.js

--- examples/cloud-memory-store/list.js
+++ examples/cloud-memory-store/list.js
@@ -1,15 +1,17 @@
-var request = require('request');
+var request = require("request");
 
 var options = {
-  method: 'GET',
-  url: 'http://localhost:7070/v1beta1/projects/testProject/locations/us-east-1',
+  method: "GET",
+  url: "http://localhost:7070/v1beta1/projects/testProject/locations/us-east-1",
   headers: {
-    'Cache-Control': 'no-cache',
+    "Cache-Control': 'no-cache",
   },
 };
 
 request(options, function(error, response, body) {
-  if (error) throw new Error(error);
+  if (error){ 
+    throw new Error(error);
+  }
 
   console.log(body);
 });
