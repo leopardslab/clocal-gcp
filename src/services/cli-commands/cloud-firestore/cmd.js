2fe64c35c9355548e7c53f92871b4eea8f77da7e
Shalitha Suranga <shalithasuranga@gmail.com>
2019-03-22 14:20:36 +0530

    Add firestore emulator support

---
+++ src/services/cli-commands/cloud-firestore/cmd.js
@@ -0,0 +1,87 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const chalk = require('chalk');
+const { exec } = require('child_process');
+const Configstore = require('configstore');
+const path = require('path');
+const pkg = require('../../../../package.json');
+const common = require('../common/cmd');
+const dockerImage = `cloudlibz/clocal-gcp-firestore:latest`;
+const defaultPort = 8086;
+
+const action = (cmd, first, second, command) => {
+  switch (cmd) {
+    case 'start':
+      start(command);
+      break;
+    case 'stop':
+      stop()
+      break;
+    default:
+      console.log(`command invalid ${cmd} ${first} ${second}`);
+  }
+};
+
+const start = () => {
+  console.log(common.figlet());
+  try {
+    console.log(chalk.blueBright(`Starting gcp firestore on port ${defaultPort}...`));
+    const config = new Configstore(path.join(pkg.name, '.containerList'));
+
+    exec(`docker run -p 8086:8086 -t -d ${dockerImage}`, (err, stdout, stderr) => {
+      if (err) console.log(chalk.bgRed(`failed to start\n${stderr}`));
+      config.set('firestore', stdout.trim());
+      const dockerId = config.get('firestore');
+      exec(
+        `docker exec -t -d ${dockerId} bash scripts/start.sh`,
+        (err, stdout, stderr) => {
+          if (err) console.log(chalk.bgRed(`failed to execute\n${stderr}`));
+          console.log(stdout);
+          if (!err) console.log(chalk.green.bgWhiteBright(`gcp firestore started ...`));
+        }
+      ); 
+    });
+  } catch (err) {
+    console.log(chalk.blueBright.bgRed(err));
+  }
+};
+
+const stop = () => {
+  try {
+    console.log(chalk.blueBright('stopping gcp firestore ...'));
+    const config = new Configstore(path.join(pkg.name, '.containerList'));
+    const dockerId = config.get('firestore');
+
+    if (dockerId) {
+      exec(`docker container rm -f ${dockerId}`, (err, stdout, stderr) => {
+        if (err) console.log(chalk.bgRed(`failed to stop\n${stderr}`));
+
+        if (dockerId === stdout.trim()) {
+          console.log(
+            chalk.green.bgWhiteBright(`stopped gcp firestore. ${dockerId}`)
+          );
+        } else {
+          console.log(
+            chalk.blueBright.bgRed(
+              'something unintended happen. Try to manually stop the docker containers ...'
+            )
+          );
+        }
+        config.delete('firestore');
+      });
+    } else {
+      console.log(
+        chalk.blueBright.bgRed('clocal gcp firestore is not running ...')
+      );
+    }
+  } catch (err) {
+    console.log(chalk.blueBright.bgRed(err));
+  }
+};
+
+module.exports = {
+  commandName: 'firestore <cmd> [first] [second]',
+  action: action
+};
