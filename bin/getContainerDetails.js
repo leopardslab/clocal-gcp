2a188cae0c41def9a34b992c5f6048d23fa6817a
sachith-1 <cst17043@std.uwu.ac.lk>
2021-04-02 20:29:15 +0530

    getContainer function removed

--- bin/getContainerDetails.js
+++ bin/getContainerDetails.js
@@ -2,13 +2,14 @@ const { exec } = require('child_process');
 
 const getContainerDetails = dockerId=>{
     exec(`docker continer inspect -f '{{.State.Running}}' ${dockerId}`,(err,stdout,stderr)=>{
-        if(err){
-            return 'Error while executing';
-        }
-        if(stdout.trim()){
-            return 'Container is runnig';
-        }
-        return 'Container is not runnig';
+        // if(err){
+        //     return 'Error while executing';
+        // }
+        // if(stdout.trim()){
+        //     return 'Container is runnig';
+        // }
+        // return 'Container is not runnig';
+        return stdout.trim();
     });
 };
 
